c2e152964ab1a676810f3ee97c4e828f19536d43
emilly.lamotte <emilly.lamotte@furg.br>
2025-02-25 17:48:53 -0300

    Updates

--- declarations.d.ts
+++
@@ -1,7 +0,0 @@
-// src/declarations.d.ts ou src/types/declarations.d.ts
-declare module "*.svg" {
-    import { FunctionComponent, SVGProps } from "react";
-    const content: FunctionComponent<SVGProps<SVGSVGElement>>;
-    export default content;
-  }
-  
